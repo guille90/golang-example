43f161759d8025ed6bf143e67046c27731431480
Guillermo Fernandez Gallardo <guille90@users.noreply.github.com>
2018-09-02 19:13:06 -0500

    remove bin dir before recompile

--- deploy-serverless-windows/build.sh
+++ deploy-serverless-windows/build.sh
@@ -1,12 +1,18 @@
+rm -rf bin
+
 #dep install
 dep ensure -v
+
 #generate binary
 env GOOS=linux go build -ldflags="-s -w" -o bin/hello hello/main.go
 env GOOS=linux go build -ldflags="-s -w" -o bin/world world/main.go
+
 #zip lambda
-#~/Go/Bin/build-lambda-zip.exe -o main.zip main
+~/Go/Bin/build-lambda-zip.exe -o main.zip main
+
 cd bin
 ~/Go/Bin/win-go-zipper.exe -o serverless-test.zip ./
-#deploy serverless
+
+# #deploy serverless
 cd ..
 sls deploy --aws-profile deployDev
